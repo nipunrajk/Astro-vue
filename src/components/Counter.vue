1401fc92a2c57a68b1410e68e76d6b096cffc5b8
Nipunraj <nipunraj99@gmail.com>
2024-11-04 00:07:21 +0530

    added intractivity to astronut

---
+++ src/components/Counter.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from "vue";
+const props = defineProps({
+  initialCount: Number,
+});
+const counter = ref(props.initialCount ?? 0);
+</script>
+
+<template>
+  <slot name="named"></slot>
+  <button @click="($event) => counter++">{{ counter }}</button>
+  <slot></slot>
+</template>
